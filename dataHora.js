daed50cd9f9b35ee6ec4234c6e75fc4d164bad8e
ElismarSilva <elismar.timaosilva@gmail.com>
2024-08-23 08:04:43 -0300

    Atualização

--- dataHora.js
+++ dataHora.js
@@ -1,10 +1,15 @@
-// dataHora.js
 const datahora = {
     dataHoraAtual: function() {
         const now = new Date();
-        const data = now.toLocaleDateString('pt-BR');  // Formato dd/mm/aaaa
-        const hora = now.toLocaleTimeString('pt-BR');  // Formato hh:mm:ss
-        return `${data} - ${hora}`;
+        const dia = String(now.getDate()).padStart(2, '0');
+        const mes = String(now.getMonth() + 1).padStart(2, '0'); // Janeiro é 0
+        const ano = now.getFullYear();
+        const horas = String(now.getHours()).padStart(2, '0');
+        const minutos = String(now.getMinutes()).padStart(2, '0');
+        const segundos = String(now.getSeconds()).padStart(2, '0');
+
+        // Retorna data no formato dd/mm/aaaa e hora no formato hh:mm:ss
+        return `${dia}/${mes}/${ano} - ${horas}:${minutos}:${segundos}`;
     }
 };
 
